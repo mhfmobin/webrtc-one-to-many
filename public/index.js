6a2b1a511b7622f469852ea688ef875bfce89ad4
Mohammad Hossein Farahmand <44579251+mhfmobin@users.noreply.github.com>
2024-07-06 03:22:10 +0330

    changed stun servers

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ function createPeer() {
     const peer = new RTCPeerConnection({
         iceServers: [
             {
-                urls: "stun:stun.stunprotocol.org"
+                urls: "stun:stun.l.google.com:19302"
             }
         ]
     });
